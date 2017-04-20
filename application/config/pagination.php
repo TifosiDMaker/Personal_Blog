b05188e6b644aa8999a9cfe3782feca95a22883d
TifosiDMaker <tifosi19921030@gmail.com>
2017-04-20 10:25:37 +0800

    first commit

---
+++ application/config/pagination.php
@@ -0,0 +1,6 @@
+<?php
+	$config['page_query_string'] = TRUE;
+
+	$config['full_tag_open'] = '<nav aria-label="Page navigation"><ul class="pagination">';
+	$config['full_tag_open'] = '</ul></nav>'
+?>
